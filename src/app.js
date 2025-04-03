f4d597dcd45f75838c98dfaf976a682d5118c366
allanmosesfernandes <fernzallan14@gmail.com>
2025-04-03 21:30:59 +0100

    init

--- src/app.js
+++ src/app.js
@@ -2,4 +2,9 @@ const express = require('express');
 
 const app = express();
 
+app.get('/:wildcard', (req, res) => {
+    const value = req.params.wildcard;
+    res.json({ value })
+})
+
 app.listen(3000);
