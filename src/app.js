49ff7bf5020244eb4031d0e189c128a7e2f0689d
allanmosesfernandes <fernzallan14@gmail.com>
2025-05-24 13:41:09 +0100

    cors

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const userRouter = require('./routes/users');
 const cors = require('cors');
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'http://13.53.143.156:80',
   credentials: true, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
